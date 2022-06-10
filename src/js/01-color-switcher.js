aa5af2d057de4bdae51ebb541dbed12599fd9b0d
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-06-11 01:18:37 +0300

    task 1 - color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+};
+
+let intervalId = null;
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', onChangeColor);
+refs.stopBtn.addEventListener('click', onStopChangeColor);
+
+
+function onChangeColor() {
+     intervalId = setInterval(() => {
+        document.querySelector("body").style.backgroundColor = getRandomHexColor();
+     }, 1000);
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+};
+
+function onStopChangeColor() {
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
